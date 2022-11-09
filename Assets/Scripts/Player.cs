83bd5aadce62e37f4e6bd1bccf55f8374d1e19f7
Rasheeq Rahman <rasheeq098@gmail.com>
2022-11-09 10:59:23 -0500

    added horizontal player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D playerRigidBody;
+
+    //Player Stats
+    [SerializeField] private float maxSpeed = 3f;
+   
+    //Singleton
+    private static Player instance;
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType<Player>();
+            return instance;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * maxSpeed, 0);
     }
 }
