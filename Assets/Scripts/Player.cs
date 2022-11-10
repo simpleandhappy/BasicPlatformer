60641cb606ae5f3126ed235a13576e9b7a6df42e
Rasheeq Rahman <rasheeq098@gmail.com>
2022-11-10 10:24:16 -0500

    added player walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody;
+    private Animator playerAnimator;
+    private Vector2 playerDirection;
+    private Vector2 forward = new Vector2(1, 1);
+    private Vector2 backward = new Vector2(-1, 1);
 
     //Player Stats
-    [SerializeField] private float maxSpeed = 3f;
+    [SerializeField] private float maxSpeed = 10f;
    
     //Singleton
     private static Player instance;
@@ -24,11 +26,26 @@ public class Player : MonoBehaviour
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerDirection = forward;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * maxSpeed, 0);
+        playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * maxSpeed, playerRigidBody.velocity.y);
+        
+        bool isWalking = playerRigidBody.velocity.x != 0;
+        playerAnimator.SetBool("Walk", isWalking);
+
+        if (playerRigidBody.velocity.x < -0.01) {
+            playerDirection = backward;
+        } 
+        else if (playerRigidBody.velocity.x > 0.01){
+            playerDirection = forward;
+        }
+        transform.localScale = playerDirection;
+        
     }
 }
