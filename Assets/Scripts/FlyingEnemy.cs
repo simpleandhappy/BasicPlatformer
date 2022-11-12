ad9e122d344ce1037a2d88a8c777d28968eda8b3
Rasheeq Rahman <rasheeq098@gmail.com>
2022-11-12 11:29:59 -0500

    serialized fly stats

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private int damage = 100;
-    private Vector3 _startPosition;
-    private Vector2 _direction = Vector2.up;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private int _damage = 100;
+    [SerializeField] private float _maxDistance = 2f;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    private Vector2 _startPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +17,18 @@ public class FlyingEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(_direction * speed * Time.deltaTime);
+        transform.Translate(_direction.normalized * _speed * Time.deltaTime);
         var distance = Vector2.Distance(_startPosition, transform.position);
-        if (distance >= 2){
+
+        if (distance >= _maxDistance){//bug where if too far will continously change direction
+            transform.position = _startPosition + (_direction.normalized * _maxDistance);
             _direction *= -1;
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject == Player.Instance.gameObject) {
-            Player.Instance.health -= damage;
+            Player.Instance.health -= _damage;
             Debug.Log($"Player Health: {Player.Instance.health}");
         }
     }
